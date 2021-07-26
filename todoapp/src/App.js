df46796712d02537e54e59a85c09e6b31ba6b62d
Andy Hu <huandy01@gmail.com>
2021-07-26 15:47:21 +0200

    Input & Liste impl.

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+//Import components
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+//https://www.youtube.com/watch?v=pCA4qpQDZD8&t=873s (19:57)
+
 function App() {
+  //Input, Funkt., die Input ändert
+  const [textInput, setTextInput] = useState("");
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>Hudo List</h1>
+      </header>
+      <Form/>
+      <TodoList/>
     </div>
   );
 }
---
+++ todoapp/src/components/Form.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Form = () => {
+    //Here i can run JS and Func.
+    const inputTextHandler = (event) => {
+      console.log(event.target.value);
+    }
+
+    return(
+    <form>
+      <input onChange={inputTextHandler} type="text" className="todo-input" />
+      <button className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+    </form>
+    );
+}
+
+export default Form;
